84fed6a579305008f1755eaff2d69b87970e4f46
keston <kestonorherhe@gmail.com>
2018-11-09 01:06:21 +0000

    this is my first commit
    commit

---
+++ precache-manifest.faede743c1c8d3f50bde5427a99ee035.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "12f4ed738e0a4da7e940",
+    "url": "/static/js/main.12f4ed73.chunk.js"
+  },
+  {
+    "revision": "a909f42bcaef7b40afc0",
+    "url": "/static/js/1.a909f42b.chunk.js"
+  },
+  {
+    "revision": "374514c6f1603cfcb9db3101a8b51206",
+    "url": "/index.html"
+  }
+];
